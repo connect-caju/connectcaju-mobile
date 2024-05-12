506b607dc14d480d1662d6b89e7d77f8c6823a8c
musekwa <musekwa2011@gmail.com>
2024-05-12 07:25:29 +0200

    Bottom tabs icons included: changes accepted

--- app/navigators/tabs/bottom-tabs-navigator.tsx
+++ app/navigators/tabs/bottom-tabs-navigator.tsx
@@ -6,6 +6,10 @@ import ActorRegistrationStackComponent from '../stacks/actor-registration-stack'
 import CommercialTransactionsStackComponent from '../stacks/commercial-transactions-stack';
 import UserProfileStackComponent from '../stacks/user-profile-stack';
 import DashboardStackComponent from '../stacks/dashboard-stack';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Feather from "react-native-vector-icons/Feather";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Octicons from "react-native-vector-icons/Octicons";
 
 const Tabs = createBottomTabNavigator();
 
@@ -40,18 +44,50 @@ export default function BottomTabsNavigator() {
             },
         })}>
         <Tabs.Screen
+          options={({route})=>({title: 'Painel', tabBarIcon: (tabInfo) => (
+            tabInfo.focused ? (
+                <MaterialCommunityIcons name="view-grid" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"}  />
+            )
+            : (
+                <MaterialCommunityIcons name="view-grid-outline" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"} />
+            )
+          )})}
           name="DashboardStack"
           component={DashboardStackComponent}
         />
         <Tabs.Screen
+          options={({route})=>({title: 'Registo', tabBarIcon: (tabInfo) => (
+            tabInfo.focused ? (
+                <MaterialIcons name="add-box" size={35} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"}  />
+            )
+            : (
+                <Octicons name="diff-added" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"} />
+            )
+          )})}
           name="ActorCategoriesStack"
           component={ActorRegistrationStackComponent}
         />
         <Tabs.Screen
+          options={({route})=>({title: 'Transações', tabBarIcon: (tabInfo) => (
+            tabInfo.focused ? (
+                <MaterialIcons name="track-changes" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"}  />
+            )
+            : (
+                <Feather name="dollar-sign" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"} />
+            )
+          )})}
           name="CommercialTransactionsStack"
           component={CommercialTransactionsStackComponent}
         />
         <Tabs.Screen
+          options={({route})=>({title: 'Você', tabBarIcon: (tabInfo) => (
+            tabInfo.focused ? (
+                <Feather name="user" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"}  />
+            )
+            : (
+                <Feather name="user" size={30} color={tabInfo.focused ? "#008000" : colorScheme === "dark" ? "#ffffff" : "#000000"} />
+                )
+          )})}
           name="UserProfilesStack"
           component={UserProfileStackComponent}
         />
